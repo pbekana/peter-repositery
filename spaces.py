09bb47f1a2307106c25f483a733166a23428a676
Peter Bekana <programmingwithbekanapeter@gmail.com>
2024-08-28 22:25:18 +0300

    second commit

---
+++ spaces.py
@@ -0,0 +1,40 @@
+# counting the number of spaces
+#a = input()
+#count = 0
+#for c in a:
+   #  count += c.count('')
+    # print('number of spaces in a string:',count)
+#text = input()
+#spaces = 0
+#for char in text:
+    #if char == ' ':
+        #spaces +=1
+#print('number of spaces:',spaces)
+# counting even numbers
+#n1 = int(input('enter the first number'))
+#n2 = int(input('enter the last number'))
+#print('number of even number between 0 and 10 is :')
+#for  n1 in range(n2):    
+ #if n1 % 2 == 0:
+      # print(n1)
+# list of even numbers
+#print('the numbers that are even are :')
+count_even = 0
+#for n1 in range(n2):
+    #if n1 % 2 == 0:
+     # count_even +=1
+#print(count_even)
+# to dislay both ends of numbers or to display all numbers un lists
+#start= int(input())
+#end = int (input())
+#for  number in range(end+1,start+1,-1): 
+  #if number % 2 == 1:
+   # print(number)
+hour = 8
+minute = 0
+while hour <= 9:
+  while minute <= 59:
+    print(hour,":",minute)  
+minute += 30
+hour += 1
+ 
---
+++ while.py
@@ -0,0 +1,90 @@
+#n = 5
+#for i in range(5):
+#  for j in range(i+1):
+#    print("8",end="\t")
+
+#  print()
+# to print the inverted part
+#n = 5
+#for i in range(n):
+#   for j in range(i,n):
+#       print('*',end='\t')
+
+#  print()
+# to print right angle triangle
+n = 5
+#for i in range(n):
+#  for j in range(i,n):
+#    print('',end="\t")
+#  for j in range(i+1):
+#      print('*',end='\t')
+
+#  print()
+# n = 5
+#for i in range(n):
+#  for j in range(i+1):
+#     print('',end='\t')
+#  for j in range(i,n):
+#     print('*',end='\t')
+
+#  print()
+
+n = 5
+# to print hill pattern
+#for i in range(n):
+#  for j in range(i,n):
+#    print('',end='\t')
+#  for j in range(i):
+#    print('*',end='\t')
+#  for j in range(i+1):
+#    print('*',end='\t')
+
+#  print()
+n = 5
+#for i in range(n):
+#  for j in range(i+1):
+#    print('',end='\t')
+#  for j in range(i,n-1):
+#    print('*',end='\t')
+#  for j in range(i,n):
+#    print('*',end='\t')
+
+#  print()
+# to print diamond pattern
+#n = 5
+#for i in range(n-1):
+#  for j in range(i,n):
+#    print('',end='\t')
+#  for j in range(i):
+#      print('*',end='\t')
+#  for j in range(i+1):
+#    print('*',end='\t')
+
+#  print()
+#for i in range(n):
+#  for j in range(i+1):
+#    print('',end='\t')
+#  for j in range(i,n-1):
+#    print('*',end='\t')
+#  for j in range(i,n):
+#    print('*',end='\t')
+
+#  print()
+# to print the increasing number in the form of triangle
+n = 9
+for i in range(n,0,-1):
+  for j in range(1,i+1):
+     print(j,end='\t')
+
+  print()
+
+
+
+
+
+
+
+
+
+
+
